fec3b683f09b74b127a043b179f6d2f24cfa58de
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-01 01:14:43 +0800

    feat(components): update svg icon for ThemeIconLight

--- src/components/ThemeIconLight.jsx
+++ src/components/ThemeIconLight.jsx
@@ -1,17 +1,21 @@
 const ThemeIconLight = () => {
   return (
     <svg
-      viewBox="0 0 24 24"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      className="w-4.5 text-(--COLOR-THEME-ICON) opacity-100"
+      width="22"
+      height="22"
+      viewBox="0 0 22 22"
       aria-hidden="true"
       data-testid="theme-icon-light"
     >
       <path
-        fill="currentColor"
-        d="M12 0a1 1 0 0 1 1 1v4a1 1 0 1 1-2 0V1a1 1 0 0 1 1-1ZM4.929 3.515a1 1 0 0 0-1.414 1.414l2.828 2.828a1 1 0 0 0 1.414-1.414L4.93 3.515ZM1 11a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2H1ZM18 12a1 1 0 0 1 1-1h4a1 1 0 1 1 0 2h-4a1 1 0 0 1-1-1ZM17.657 16.243a1 1 0 0 0-1.414 1.414l2.828 2.828a1 1 0 1 0 1.414-1.414l-2.828-2.828ZM7.757 17.657a1 1 0 1 0-1.414-1.414L3.515 19.07a1 1 0 1 0 1.414 1.414l2.828-2.828ZM20.485 4.929a1 1 0 0 0-1.414-1.414l-2.828 2.828a1 1 0 1 0 1.414 1.414l2.828-2.828ZM13 19a1 1 0 1 0-2 0v4a1 1 0 1 0 2 0v-4ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Z"
-      ></path>
+        d="M20.125 11.877A7.333 7.333 0 1 1 10.124 1.875a9.168 9.168 0 1 0 10.001 10.002Z"
+        stroke="#091540"
+        stroke-width="1.98"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        fill="none"
+      />
     </svg>
   );
 };
