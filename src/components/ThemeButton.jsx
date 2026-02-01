88de820f78e93382bcfe4f075f559fa5a8bb46b8
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-01 23:17:57 +0800

    feat(styles): add red outline on focus to ThemeButton

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -5,7 +5,7 @@ const ThemeButton = ({ onThemeChange, theme, children }) => {
 
   return (
     <button
-      className="relative flex h-full w-full cursor-pointer items-center justify-center py-3.25"
+      className="flex h-full w-full cursor-pointer items-center justify-center rounded-xl py-3.25 focus-visible:outline-2 focus-visible:outline-offset-3 focus-visible:outline-(--COLOR-BUTTON-PRIMARY-FOCUS)"
       type="button"
       aria-label={label}
       aria-pressed={isPressed}
