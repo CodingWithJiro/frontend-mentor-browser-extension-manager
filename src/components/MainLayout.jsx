90f1b4d4cf52978f132e434211058ae2fccba59d
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-02 23:04:08 +0800

    feat(styles): add padding to MainLayout

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,5 +1,5 @@
 const MainLayout = ({ children }) => {
-  return <div className="min-h-screen">{children}</div>;
+  return <div className="min-h-screen px-4 py-5">{children}</div>;
 };
 
 export default MainLayout;
