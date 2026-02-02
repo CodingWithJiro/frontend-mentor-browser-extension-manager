88cdc440c07c08ff6ab50c15d8e07e09790ca62a
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-02 23:28:03 +0800

    feat(styles): add background color to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Theme from "./Theme";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between border border-red-700 px-2.75 py-2">
+    <header className="flex items-center justify-between border border-red-700 bg-(--COLOR-BG-CARD-PRIMARY) px-2.75 py-2">
       <CompanyTitle />
       <Theme />
     </header>
