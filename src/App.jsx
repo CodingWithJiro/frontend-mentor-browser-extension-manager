c0e4162bb5f4902c8944112a36303a08cb292c61
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-02 22:55:56 +0800

    feat(components): create MainLayout and import it in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import SkipLink from "./components/SkipLink";
 import Theme from "./components/Theme";
+import MainLayout from "./components/MainLayout";
 
 function App() {
   return (
-    <>
+    <MainLayout>
       <SkipLink />
       <Theme />
-    </>
+    </MainLayout>
   );
 }
 
