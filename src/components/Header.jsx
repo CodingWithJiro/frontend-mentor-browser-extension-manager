54c4a316051512cfdddd637dd310098ccf88bd7d
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-03 00:36:32 +0800

    feat(styles)C: adjust Header padding and border radius on larger screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Theme from "./Theme";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between rounded-lg bg-(--COLOR-BG-CARD-PRIMARY) px-2.75 py-2 shadow-(--SHADOW-CARD-PRIMARY) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out">
+    <header className="flex items-center justify-between rounded-lg bg-(--COLOR-BG-CARD-PRIMARY) px-2.75 py-2 shadow-(--SHADOW-CARD-PRIMARY) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out md:rounded-2xl md:px-3.5 md:py-3">
       <CompanyTitle />
       <Theme />
     </header>
