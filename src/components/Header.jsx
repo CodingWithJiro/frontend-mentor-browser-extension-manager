478422b0ced80720a56b9c957d5715c3894f7fd5
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-03 00:23:56 +0800

    feat(styles): add box shadow to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Theme from "./Theme";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between border border-red-700 bg-(--COLOR-BG-CARD-PRIMARY) px-2.75 py-2">
+    <header className="flex items-center justify-between rounded-lg bg-(--COLOR-BG-CARD-PRIMARY) px-2.75 py-2 shadow-(--SHADOW-CARD-PRIMARY) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out">
       <CompanyTitle />
       <Theme />
     </header>
