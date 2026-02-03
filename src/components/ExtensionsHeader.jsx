6221a4a1b68b39b935695d37b1e2ee5846b6aa8c
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-03 23:52:12 +0800

    feat(styles): add mobile style for ExtensionsHeader title

--- src/components/ExtensionsHeader.jsx
+++ src/components/ExtensionsHeader.jsx
@@ -2,8 +2,10 @@ import ExtensionsFilter from "./ExtensionsFilter";
 
 const ExtensionsHeader = ({ filter, setFilter }) => {
   return (
-    <header>
-      <h2>Extensions List</h2>
+    <header className="flex flex-col items-center">
+      <h2 className="text-[2.15rem] font-bold text-(--COLOR-TEXT-PRIMARY) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out">
+        Extensions List
+      </h2>
       <ExtensionsFilter filter={filter} setFilter={setFilter} />
     </header>
   );
