368dcac940940abf927a72df5c103a2d687ad91a
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-04 22:29:43 +0800

    feat(components): add extensions-title id to h2 tag in ExtensionssHeader

--- src/components/ExtensionsHeader.jsx
+++ src/components/ExtensionsHeader.jsx
@@ -3,7 +3,10 @@ import ExtensionsFilter from "./ExtensionsFilter";
 const ExtensionsHeader = ({ filter, setFilter }) => {
   return (
     <header className="flex flex-col items-center gap-4 md:flex-row md:justify-between">
-      <h2 className="text-[2.15rem] font-bold text-(--COLOR-TEXT-PRIMARY) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out">
+      <h2
+        className="text-[2.15rem] font-bold text-(--COLOR-TEXT-PRIMARY) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out"
+        id="extensions-title"
+      >
         Extensions List
       </h2>
       <ExtensionsFilter filter={filter} setFilter={setFilter} />
