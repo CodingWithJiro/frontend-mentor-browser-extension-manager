00817d58bafbff66edf75a89a14e6fff47603ec5
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-04 20:01:38 +0800

    feat(styles): make ExtensionsHeader responsive for tablet size screens

--- src/components/ExtensionsHeader.jsx
+++ src/components/ExtensionsHeader.jsx
@@ -2,7 +2,7 @@ import ExtensionsFilter from "./ExtensionsFilter";
 
 const ExtensionsHeader = ({ filter, setFilter }) => {
   return (
-    <header className="flex flex-col items-center gap-4">
+    <header className="flex flex-col items-center gap-4 md:flex-row md:justify-between">
       <h2 className="text-[2.15rem] font-bold text-(--COLOR-TEXT-PRIMARY) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out">
         Extensions List
       </h2>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Theme from "./Theme";
 
 const Header = () => {
   return (
-    <header className="mb-8 flex items-center justify-between rounded-lg bg-(--COLOR-BG-CARD-PRIMARY) px-2.75 py-2 shadow-(--SHADOW-CARD-PRIMARY) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out md:rounded-2xl md:px-3.5 md:py-3">
+    <header className="mb-8 flex items-center justify-between rounded-lg bg-(--COLOR-BG-CARD-PRIMARY) px-2.75 py-2 shadow-(--SHADOW-CARD-PRIMARY) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out md:mb-16 md:rounded-2xl md:px-3.5 md:py-3">
       <CompanyTitle />
       <Theme />
     </header>
