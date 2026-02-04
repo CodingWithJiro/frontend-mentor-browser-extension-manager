f517049681d0750688a187be470dbb34bb8af032
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-04 22:56:26 +0800

    feat(styles): add responsive grid layout to ExtensionsGrid

--- src/components/ExtensionsGrid.jsx
+++ src/components/ExtensionsGrid.jsx
@@ -2,7 +2,10 @@ import Extension from "./Extension";
 
 const ExtensionsGrid = () => {
   return (
-    <section aria-labelledby="extensions-title">
+    <section
+      aria-labelledby="extensions-title"
+      className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+    >
       <Extension />
     </section>
   );
