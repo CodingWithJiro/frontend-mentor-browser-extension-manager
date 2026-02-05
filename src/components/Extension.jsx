cf402d45001072548358ff2a1c9fc3607642d7c3
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-05 23:38:49 +0800

    feat(styles): add background to remove button in Extension

--- src/components/Extension.jsx
+++ src/components/Extension.jsx
@@ -18,7 +18,7 @@ const Extension = ({ logo, name, description }) => {
 
       <div className="flex items-center justify-between">
         <button
-          className="cursor-pointer rounded-full border border-solid border-(--COLOR-BORDER-SECONDARY) px-4 py-1.75 text-[0.95rem] font-medium select-none motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out"
+          className="cursor-pointer rounded-full border border-solid border-(--COLOR-BORDER-SECONDARY) bg-(--COLOR-BUTTON-TERTIARY) px-4 py-1.75 text-[0.95rem] font-medium select-none motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out"
           type="button"
         >
           Remove
