694cd725d79ed8367e0055c4e788dd440851e681
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-09 22:35:15 +0800

    refactor: remove x close button and list element in RemoveModal

--- src/components/RemoveModal.jsx
+++ src/components/RemoveModal.jsx
@@ -1,24 +1,16 @@
 const RemoveModal = ({ name }) => {
   return (
     <dialog>
-      <form method="dialog">
-        <button type="submit">X</button>
-      </form>
-
       <section>
         <h2>Remove Extension</h2>
 
         <p>Remove {name} from your extension list?</p>
 
-        <ul>
-          <li>
-            <button type="button">Confirm</button>
-          </li>
+        <div></div>
 
-          <li>
-            <button type="button">Cancel</button>
-          </li>
-        </ul>
+        <button type="button">Confirm</button>
+
+        <button type="button">Cancel</button>
       </section>
     </dialog>
   );
