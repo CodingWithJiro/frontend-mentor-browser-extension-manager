5b35f66562c9fcabb1af8d1fd6e6c62cfb347e50
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-15 01:40:08 +0800

    feat(styles): add style to RestoreModal

--- src/components/RestoreModal.jsx
+++ src/components/RestoreModal.jsx
@@ -28,31 +28,52 @@ const RestoreModal = ({
       ref={dialogRef}
       onClose={() => setShowRestore(false)}
       aria-labelledby="restore-modal-heading"
+      className="hide-scrollbar m-auto max-h-100 max-w-72 rounded-2xl bg-(--COLOR-BG-CARD-PRIMARY) px-4.5 py-5 font-(family-name:--FF) shadow-(--SHADOW-CARD-SECONDARY) md:max-w-98"
     >
-      <h2 id="restore-modal-heading">Recently Removed</h2>
+      <h2
+        id="restore-modal-heading"
+        className="mb-5 text-center text-[1.25rem] leading-tight font-bold text-(--COLOR-TEXT-PRIMARY)"
+      >
+        Recently Removed
+      </h2>
 
       {isEmpty && <p>All extensions restored.</p>}
 
-      <ul>
+      <ul className="mb-2.5 flex flex-col gap-2">
         {[...removedExtensions].reverse().map(({ logo, name, description }) => {
           return (
-            <li key={name}>
-              <img src={logos[logo]} alt="" width="60" height="60" />
-              <p>{name}</p>
-              <p>{description}</p>
-              <button
-                type="button"
-                onClick={() => handleRestore(name)}
-                aria-label={`Restore ${name}`}
-              >
-                Restore
-              </button>
+            <li key={name} className="flex flex-col gap-1">
+              <div className="flex items-center justify-between gap-2">
+                <div className="flex items-center justify-start gap-2">
+                  <img src={logos[logo]} alt="" width="55" height="55" />
+                  <div>
+                    <p className="text-[0.9rem]/[1.25] font-medium text-(--COLOR-TEXT-PRIMARY)">
+                      {name}
+                    </p>
+                    <p className="text-[0.70rem] text-(--COLOR-TEXT-SECONDARY)">
+                      {getShortDescription(description)}
+                    </p>
+                  </div>
+                </div>
+                <button
+                  type="button"
+                  onClick={() => handleRestore(name)}
+                  aria-label={`Restore ${name}`}
+                  className="bg-(-COLOR-BUTTON-PRIMARY) cursor-pointer rounded-full border border-solid border-(--COLOR-BORDER-SECONDARY) px-2.25 py-1 text-sm text-(--COLOR-TEXT-SECONDARY) select-none hover:border-(--COLOR-BORDER-TERTIARY) hover:bg-(--COLOR-BUTTON-TERTIARY-FOCUS) hover:text-(--COLOR-TEXT-PRIMARY) focus-visible:border-(--COLOR-BORDER-SECONDARY-FOCUS) focus-visible:bg-(--COLOR-BUTTON-TERTIARY-FOCUS) focus-visible:text-(--COLOR-TEXT-PRIMARY) focus-visible:outline-2 focus-visible:outline-offset-3 focus-visible:outline-(--COLOR-OUTLINE-PRIMARY) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out"
+                >
+                  Restore
+                </button>
+              </div>
             </li>
           );
         })}
       </ul>
 
-      <button type="button" onClick={() => setShowRestore(false)}>
+      <button
+        type="button"
+        onClick={() => setShowRestore(false)}
+        className="w-full cursor-pointer rounded-full border border-solid border-(--COLOR-BORDER-SECONDARY) bg-(--COLOR-BUTTON-TERTIARY) px-2.25 py-1 text-[0.95rem] font-medium text-(--COLOR-TEXT-PRIMARY) outline-transparent select-none hover:border-(--COLOR-BORDER-TERTIARY) hover:bg-(--COLOR-BUTTON-TERTIARY-FOCUS) focus-visible:border-(--COLOR-BORDER-SECONDARY-FOCUS) focus-visible:bg-(--COLOR-BUTTON-TERTIARY-FOCUS) focus-visible:outline-2 focus-visible:outline-offset-3 focus-visible:outline-(--COLOR-OUTLINE-PRIMARY) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out"
+      >
         Close
       </button>
     </dialog>
