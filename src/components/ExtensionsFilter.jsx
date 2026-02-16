b693dad7720c697ac781352742967d60b12adf32
CodingWithJiro <chavezelmar03@gmail.com>
2026-02-16 19:51:40 +0800

    feat(a11y): add aria-label and aria-pressed to ul and button elements in ExtensionFilter

--- src/components/ExtensionsFilter.jsx
+++ src/components/ExtensionsFilter.jsx
@@ -2,13 +2,14 @@ const ExtensionsFilter = ({ filter, setFilter }) => {
   const FILTERS = ["all", "active", "inactive"];
 
   return (
-    <ul className="flex gap-3.75">
+    <ul className="flex gap-3.75" aria-label="Extensions filter">
       {FILTERS.map((value) => {
         return (
           <li key={value}>
             <button
               type="button"
               onClick={() => setFilter(value)}
+              aria-pressed={filter === value}
               className={`cursor-pointer rounded-full px-[1em] py-[0.425em] font-(family-name:--FF) text-[1.2rem] font-normal outline-transparent select-none focus-visible:outline-2 focus-visible:outline-offset-3 focus-visible:outline-(--COLOR-OUTLINE-PRIMARY) motion-safe:transition-colors motion-safe:duration-300 motion-safe:ease-in-out ${filter === value ? "bg-(--COLOR-BG-CARD-SECONDARY-ACTIVE) text-(--COLOR-TEXT-PRIMARY-ACTIVE) hover:bg-(--COLOR-BG-CARD-SECONDARY-ACTIVE-EMPHASIS) focus-visible:bg-(--COLOR-BG-CARD-SECONDARY-ACTIVE-EMPHASIS)" : "border-(--BORDER-CARD-PRIMARY) bg-(--COLOR-BG-CARD-SECONDARY) text-(--COLOR-TEXT-PRIMARY) shadow-(--SHADOW-CARD-SECONDARY) hover:bg-(--COLOR-BG-CARD-SECONDARY-HOVER) focus-visible:bg-(--COLOR-BG-CARD-SECONDARY-HOVER)"} capitalize`}
             >
               {value}
